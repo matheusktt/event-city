0b961bad39a90448ec62b49f888dee0c025cf229
matheusktt <matheus_0119@outlook.com>
2023-02-24 00:16:27 -0300

    resources

---
+++ src/main/java/com/devsuperior/bds02/resources/CityResource.java
@@ -0,0 +1,10 @@
+package com.devsuperior.bds02.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/cities")
+public class CityResource {
+
+}
