9faddd6c6ebd98093ee1da8461664ccd06060a5e
matheusktt <matheus_0119@outlook.com>
2023-02-25 15:51:41 -0300

    event features

--- src/main/java/com/devsuperior/bds02/resources/EventResource.java
+++ src/main/java/com/devsuperior/bds02/resources/EventResource.java
@@ -1,10 +1,32 @@
 package com.devsuperior.bds02.resources;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.devsuperior.bds02.dto.EventDTO;
+import com.devsuperior.bds02.services.EventService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/events")
 public class EventResource {
+    @Autowired
+    private EventService service;
 
+    @GetMapping
+    public ResponseEntity<List<EventDTO>> findAll() {
+
+        List<EventDTO> list = service.findAll();
+
+        return ResponseEntity.ok().body(list);
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<EventDTO> update(@PathVariable Long id, @RequestBody EventDTO dto) {
+
+        dto = service.update(id, dto);
+
+        return ResponseEntity.ok().body(dto);
+    }
 }
--- src/main/java/com/devsuperior/bds02/services/EventService.java
+++ src/main/java/com/devsuperior/bds02/services/EventService.java
@@ -1,8 +1,47 @@
 package com.devsuperior.bds02.services;
 
+import com.devsuperior.bds02.dto.EventDTO;
+import com.devsuperior.bds02.entities.City;
+import com.devsuperior.bds02.entities.Event;
+import com.devsuperior.bds02.repositories.EventRepository;
+import com.devsuperior.bds02.services.exceptions.ResourceNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityNotFoundException;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class EventService {
+    @Autowired
+    private EventRepository repository;
 
+    @Transactional(readOnly = true)
+    public List<EventDTO> findAll() {
+
+        List<Event> list = repository.findAll();
+
+        return list.stream().map(event -> new EventDTO(event)).collect(Collectors.toList());
+    }
+
+    @Transactional
+    public EventDTO update(Long id, EventDTO dto) {
+
+        try {
+            Event entity = repository.getOne(id);
+
+            entity.setName(dto.getName());
+            entity.setDate(dto.getDate());
+            entity.setUrl(dto.getUrl());
+            entity.setCity(new City(dto.getCityId(), null));
+            entity = repository.save(entity);
+
+            return new EventDTO(entity);
+        } catch (EntityNotFoundException error) {
+            throw new ResourceNotFoundException("Id not found: " + id);
+        }
+
+    }
 }
