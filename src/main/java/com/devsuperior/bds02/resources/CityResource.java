8dc6bd7d69ca448a1ee6dd278ff46df935ab8b4d
matheusktt <matheus_0119@outlook.com>
2023-02-25 15:51:29 -0300

    city features

--- src/main/java/com/devsuperior/bds02/resources/CityResource.java
+++ src/main/java/com/devsuperior/bds02/resources/CityResource.java
@@ -1,10 +1,44 @@
 package com.devsuperior.bds02.resources;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.devsuperior.bds02.dto.CityDTO;
+import com.devsuperior.bds02.services.CityService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequestMapping("/cities")
 public class CityResource {
 
+    @Autowired
+    private CityService service;
+
+    @GetMapping
+    public ResponseEntity<List<CityDTO>> findAll() {
+
+        List<CityDTO> list = service.findAll();
+
+        return ResponseEntity.ok().body(list);
+    }
+
+    @PostMapping
+    public ResponseEntity<CityDTO> insert(@RequestBody CityDTO dto) {
+
+        dto = service.insert(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
+
+        return ResponseEntity.created(uri).body(dto);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+
+        service.delete(id);
+
+        return ResponseEntity.noContent().build();
+    }
 }
